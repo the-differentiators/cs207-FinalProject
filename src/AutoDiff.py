b20cf579c8ab547d986e512c54c2ddf8226e6f31
mscott935 <mscott935.ms@gmail.com>
2019-11-11 19:09:26 -0500

    Ad_var skeleton v1

---
+++ src/AutoDiff.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+class Ad_Var():
+    def __init__(self, val, varname):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __str__(self):
+        pass
+
+    def get_val(self):
+        pass
+
+    def get_ders(self, wrt_var=None):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __radd__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __rmul__(self, other):
+        pass
+
+    def __div__(self, other):
+        pass
+
+    def __rdiv__(self, other):
+        pass
+
+    def __abs__(self):
+        pass
+
+    def __pow__(self, other):
+        pass
+
+    def exp(self):
+        pass
+
+    def log(self, logbase=np.e):
+        pass
+
+    def sin(self):
+        pass
+
+    def cos(self):
+        pass
+
+    def tan(self):
+        pass
+
+    def arcsin(self):
+        pass
+
+    def arccos(self):
+        pass
+
+    def arctan(self):
+        pass
+
+
+
+
+    
+
+    
