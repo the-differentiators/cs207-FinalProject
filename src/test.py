64631fa01f2b995828b45775a7235b853affb22b
Yiwen Wang <yiwenwang@g.harvard.edu>
2019-11-16 12:07:46 -0500

    update test.py

--- src/test.py
+++ src/test.py
@@ -10,7 +10,7 @@ import pytest
 import numpy as np
 from AutoDiff import Ad_Var
 
-class Test_functions():
+class test():
 
     def test_exp(self):
         x1 = Ad_Var(1,np.array([1, 0]))
@@ -76,3 +76,25 @@ class Test_functions():
         f = (x1+1)*x2 + x2*(x1+1)
         assert f.get_val() == 8
         assert f.get_ders() == 4 
+    
+    def test_multiple(self):
+        x = Ad_Var(1, np.array([1, 0, 0]))
+        y = Ad_Var(2, np.array([0, 1, 0]))
+        z = Ad_Var(3, np.array([0, 0, 1]))
+        f = np.array([Ad_Var.cos(x)*(y+2), 1 + z**2/(x*y*3), 3*Ad_Var.log(x*2) + Ad_Var.exp(x/z), Ad_Var.arctan(Ad_Var.arcsin(y/4))])
+        assert (Ad_Var.get_values(f) == [2.161209223472559, 2.5, 3.475053966765925, 0.48234790710102493]).all()
+        assert (Ad_Var.get_jacobian(f, 4, 3) == [[-3.365883939231586, 0.5403023058681398, 0],[-1.5, -0.75,1],[3.465204141695363,0,-0.15506804723178771],[0, 0.22656190261729092,0]]).all()
+
+if __name__ == "__main__":
+    a = test()
+    a.test_exp()
+    a.test_log()
+    a.test_trig()
+    a.test_inverse_trig()
+    a.test_pow()
+    a.test_div1()
+    a.test_div2()
+    a.test_mul1()
+    a.test_mul2()
+    a.test_multiple()
+    print("All tests passed!")
