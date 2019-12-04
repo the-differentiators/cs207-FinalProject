01b0114d1dd2deb2923e67a44ace7d3003d4b854
Michael Scott <mscott935.ms@gmail.com>
2019-12-04 14:55:13 -0500

    Duplicate parent bug fixed

--- src/ReverseAD.py
+++ src/ReverseAD.py
@@ -2,25 +2,25 @@ import numpy as np
 import numbers as numbers
 
 class rAd_Var():
-    def __init__(self, val, ders=None, original = True):
+    def __init__(self, val, ders=None):
         self._val = np.array([val]).reshape(-1,)
         self._ders = ders
         self.parents = []
         self.children = []
-        self.original = original
+        self.seen = False # Set to True during runreverse() traversal, then reset at end
 
     def __str__(self):
         return f'Reverse Autodiff Object with value {self._val} and gradient {self.gradient()}'
 
     def __add__(self, other):
         try:
-            rad_object = rAd_Var(self._val + other._val, original=False)
+            rad_object = rAd_Var(self._val + other._val)
             self.children.append((rad_object, np.ones(len(self._val))))
             other.children.append((rad_object, np.ones(len(self._val))))
             rad_object.parents = [self, other]
             return rad_object
         except AttributeError:
-            rad_object = rAd_Var(self._val + other, original=False)
+            rad_object = rAd_Var(self._val + other)
             self.children.append((rad_object, np.ones(len(self._val))))
             rad_object.parents = [self]
             return rad_object
@@ -30,13 +30,13 @@ class rAd_Var():
 
     def __sub__(self, other):
         try:
-            rad_object = rAd_Var(self._val - other._val, original=False)
+            rad_object = rAd_Var(self._val - other._val)
             self.children.append((rad_object, np.ones(len(self._val))))
             other.children.append((rad_object, -1 * np.ones(len(self._val))))
             rad_object.parents = [self, other]
             return rad_object
         except AttributeError:
-            rad_object = rAd_Var(self._val - other, original=False)
+            rad_object = rAd_Var(self._val - other)
             self.children.append((rad_object, np.ones(len(self._val))))
             rad_object.parents = [self]
             return rad_object
@@ -46,13 +46,13 @@ class rAd_Var():
 
     def __mul__(self, other):
         try:
-            rad_object = rAd_Var(self._val * other._val, original=False)
+            rad_object = rAd_Var(self._val * other._val)
             self.children.append((rad_object, other._val))
             other.children.append((rad_object, self._val))
             rad_object.parents = [self, other]
             return rad_object
         except AttributeError:
-            rad_object = rAd_Var(self._val * other, original=False)
+            rad_object = rAd_Var(self._val * other)
             self.children.append((rad_object, np.array([other] * len(self._val))))
             rad_object.parents = [self]
             return rad_object
@@ -62,12 +62,12 @@ class rAd_Var():
 
     def __truediv__(self, other):
         try:
-            rad_object = rAd_Var(self.val / other.val, original=False)
+            rad_object = rAd_Var(self.val / other.val)
             self.children.append((rad_object, 1 / other._val))
             other.children.append((rad_object, -self._val*(other._val**(-2))))
             return rad_object
         except AttributeError:
-            ad = rAd_Var(self.val / other, original=False)
+            ad = rAd_Var(self.val / other)
             self.children.append((1/other, ad))
             rad_object.parents = [self]
             return ad
@@ -77,13 +77,13 @@ class rAd_Var():
 
     def __pow__(self, other):
         try:
-            rad_object = rAd_Var(self._val ** other.val, original=False)
+            rad_object = rAd_Var(self._val ** other.val)
             self.children.append((rad_object, other.val * self._val ** (other.val - 1)))
             other.children.append((rad_object, self._val**other.val*np.log(self._val)))
             rad_object.parents = [self, other]
             return rad_object
         except AttributeError:
-            rad_object = rAd_Var(self._val ** other, original=False)
+            rad_object = rAd_Var(self._val ** other)
             self.children.append((rad_object, other * self._val ** (other - 1)))
             rad_object.parents = [self]
             return rad_object
@@ -105,13 +105,18 @@ class rAd_Var():
         return self._ders
 
     def get_originals(self):
+        # Walk through tree, finding nodes without parents
         ancestorlist = []
         if self.parents:
             for parent in self.parents:
-                ancestorlist.append(parent)
-                ancestorlist += parent.get_originals()
-        else:
-            pass
+                if not parent.seen:
+                    ancestorlist.append(parent) 
+                    ancestorlist += parent.get_originals()
+                    parent.seen = True
+        
+        # Reset all nodes in tree to unseen for future traversals
+        for ancestor in ancestorlist:
+            ancestor.seen = False
         return ancestorlist
 
     def runreverse(self):
@@ -131,15 +136,14 @@ class rAd_Var():
     def exp(self):
         rad_object = rAd_Var(np.exp(self._val))
         self.children.append((rad_object, np.exp(self._val)))
+        rad_object.parents = [self]
         return rad_object
 
 x = rAd_Var(1)
 y = rAd_Var(2)
 
+x1 = x * y
+x2 = x1.exp()
 
-z = x**2 * y
-print(z.runreverse())
-
-# a = z + rAd_Var.exp(z)
-# print(x.gradient())
-# print(y.gradient())
+f = x1 + x2
+print(f.runreverse()) 
